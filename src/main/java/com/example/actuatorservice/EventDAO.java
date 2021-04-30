7c59b0ef7c429f368c9f33f524ff61d1f2e8d9d0
Robert Heidbrink <Robert.Heidbrink@ChurchOfJesusChrist.org>
2021-04-30 12:39:27 -0600

    thirteenth!

--- src/main/java/com/example/actuatorservice/EventDAO.java
+++ src/main/java/com/example/actuatorservice/EventDAO.java
@@ -13,15 +13,20 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
 
 public class EventDAO {
-    private static final String HIGHEST_GROSSING_MOVIES = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films";
+    private static final String WIKIPEDIA = "https://en.wikipedia.org/";
+    private static final String HIGHEST_GROSSING_MOVIES = WIKIPEDIA + "wiki/List_of_highest-grossing_films";
     private static final String MOVIE_REGEX_1 = "High-grossing films by year of release.*?>";
-    private static final String MOVIE_REGEX_2 = "<.*?<a.*?>(.*?)<\\/a>";
-    private static final int MOVIE_TITLE_REGEX_GROUP = 1;
+    private static final String MOVIE_REGEX_2 = "<.*?<a.*?href=\"(.*?)\".*?>(.*?)</a>";
+    private static final String DETAILS_REGEX = "src=\"([^\"]*?\\.jpg)\".*href=\"([^\"]*?imdb[^\"]*?)\"";
+    private static final int SRC_GROUP = 1;
+    private static final int IMDB_GROUP = 2;
+    private static final int MOVIE_URL_REGEX_GROUP = 1;
+    private static final int MOVIE_TITLE_REGEX_GROUP = 2;
 
     private static final String EVENT_FILE_NAME_PREFIX = "events_of_";
 
     static YearEventsModel getEvents(int year){
-        File rawEvents = null;
+        File rawEvents;
         ClassPathResource resource = new ClassPathResource(EVENT_FILE_NAME_PREFIX + year);
 
         YearEventsModel model = null;
@@ -89,14 +94,21 @@ public class EventDAO {
             if(matcher.find()){
                 EventModel event = new EventModel();
                 event.setType(EventType.Movie);
-                event.setImgUrl("test");
-                event.setMediaUrl("test");
+                Matcher details = getDetails(matcher.group(MOVIE_URL_REGEX_GROUP));
+                if(details.find()){
+                    event.setImgUrl(details.group(SRC_GROUP));
+                    event.setMediaUrl(details.group(IMDB_GROUP));
+                }
                 event.setName(matcher.group(MOVIE_TITLE_REGEX_GROUP));
                 yearEvents.getEvents().add(event);
             }
         }
     }
 
+    private static Matcher getDetails(String wikiPage) {
+        String html = getHTML(WIKIPEDIA+wikiPage);
+        return Pattern.compile(DETAILS_REGEX).matcher(html);
+    }
 
     private static Pattern getRegexPatternForMovie(int year){
         return Pattern.compile(MOVIE_REGEX_1 + year + MOVIE_REGEX_2);
